174fa8337c71650c443122b49c4dafbd83b7f856
Juan Salvatore <juansalvatore.ar@gmail.com>
2019-09-11 22:33:25 -0300

    Agregar pruebas

--- pila/pruebas_alumno_pila.c
+++ pila/pruebas_alumno_pila.c
@@ -23,21 +23,27 @@ int toInt(void *num)
     return *(int *)num;
 }
 
-void pruebas_pila_con_dos_elementos()
+void pruebas_pila_apilar()
 {
     printf("INICIO DE PRUEBAS DE PILA CON DOS ELEMENTOS\n");
     pila_t *pila = pila_crear();
     print_test("Pila creada distinta a NULL", pila != NULL);
     int n = 33;
     int n2 = 44;
-    pila_apilar(pila, &n);
-    pila_apilar(pila, &n2);
+    print_test("apila entero 33", pila_apilar(pila, &n) == true);
+    print_test("apila entero 44", pila_apilar(pila, &n2) == true);
     print_test("desapilar pila igual a tope anterior", toInt(pila_ver_tope(pila)) == toInt(pila_desapilar(pila)));
     print_test("los elementos se mantienen al desapilar", toInt(pila_ver_tope(pila)) == 33);
     print_test("pila vacia == false", pila_esta_vacia(pila) == false);
     print_test("desapilar pila igual a tope anterior", toInt(pila_ver_tope(pila)) == toInt(pila_desapilar(pila)));
     print_test("pila esta vacia", pila_esta_vacia(pila) == true);
     print_test("desapilar pila vacía == NULL", pila_desapilar(pila) == NULL);
+
+    printf("INICIO DE PRUEBAS APILAR NULL\n");
+    pila_apilar(pila, NULL);
+    // printf("------->%p\n", pila_ver_tope(pila));
+    print_test("apila NUll", pila_apilar(pila, NULL) == true);
+    print_test("tope == NUll", pila_ver_tope(pila) == NULL);
     pila_destruir(pila);
 }
 
@@ -67,16 +73,17 @@ void pruebas_de_volumen()
     printf("-> prueba desapilar %d elementos\n", size);
     pila_mantiene_topes = true;
     bool desapila_correctamente = true;
-    for (int j = size - 1; j > 0; j--)
+    for (int j = size - 1; j >= 0; j--)
     {
         if (toInt(pila_desapilar(pila)) != j)
             desapila_correctamente = false;
-        if (toInt(pila_ver_tope(pila)) != (j - 1))
+        if (pila_ver_tope(pila) != NULL && toInt(pila_ver_tope(pila)) != (j - 1))
             pila_mantiene_topes = false;
     }
     print_test("desapilar funciona correctamente", desapila_correctamente == true);
     print_test("pila mantiene topes al desapilar", pila_mantiene_topes == true);
     print_test("al desapilar todos los elementos la pila esta vacía", pila_esta_vacia(pila) == true);
+    print_test("al desapilar todos los elementos el tope es NULL", pila_ver_tope(pila) == NULL);
 
     free(arr);
     pila_destruir(pila);
@@ -85,6 +92,6 @@ void pruebas_de_volumen()
 void pruebas_pila_alumno()
 {
     pruebas_pila_crear();
-    pruebas_pila_con_dos_elementos();
+    pruebas_pila_apilar();
     pruebas_de_volumen();
 }
